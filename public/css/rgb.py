8ea444e7cb487ef69cf93a4e1803cb4bf50b71a0
Sean Towne <seanmichaeltowne@gmail.com>
2021-02-23 19:03:47 -0800

    AutomatedCommitAndPush

--- public/css/rgb.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-im = Image.open('../green.png') # Can be many different formats.
-pix = im.load()
-print(im.size)  # Get the width and hight of the image for iterating over
-print(pix[0,0])  # Get the RGBA Value of the a pixel of an image
