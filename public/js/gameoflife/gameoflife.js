dc6cddefaa8ab84e93130a00a39a3641086e02fe
Sean Towne <seantowne@Seans-MacBook-Pro.local>
2021-05-02 20:26:50 -0700

    auto commit

--- public/js/gameoflife/gameoflife.js
+++ public/js/gameoflife/gameoflife.js
@@ -1,3 +1,6 @@
+
+
+
 class Square extends React.Component{
 	render(){
 		const color = (
@@ -20,7 +23,7 @@ class Square extends React.Component{
 
 class Board extends React.Component{
 	renderSquare(x, y){
-		var alive = this.props.lifeMatrix[y][x];
+		var alive = this.props.lifeMatrix[y][x].alive;
 
 		var key = ("(" + x.toString() + ", " + y.toString() + ")");
 		return(
@@ -61,12 +64,28 @@ class Controls extends React.Component{
 		let running = this.props.running ? 'Pause' : 'Run';
 		return(
 			<div>
+
 				<button 
 					onClick = {()=>this.props.onRunningPauseClick()}
-				>{running}</button>
+					>{running}
+				</button>
+
 				<button 
 					onClick = {()=>this.props.onClearClick()}
-				>Clear</button>
+					>Clear
+				</button>
+
+				<div id="slider">
+					<input 
+						type="range" 
+						min="1" 
+						max="100"
+						step="1"
+						onMouseUp={(event)=>this.props.onSpeedChange(event)}
+						>
+					</input>
+				</div>
+
 			</div>
 		);
 	}
@@ -76,17 +95,17 @@ class Game extends React.Component{
 	constructor(props){
 		super(props);
 
-		var width = 90;
-		var height = 54;
+		/*
+		this.width = 90;
+		this.height = 54;
+		*/
+		this.width = 40;
+		this.height = 24;
 
-		var lifeMatrix = [];
-		for (var i = 0; i < height; i++ ){
-			var row = []
-			for ( var j = 0; j < width; j++ ){
-				row.push(Math.random() > 0.5);
-			}
-			lifeMatrix.push(row);
-		}
+		
+
+
+		var lifeMatrix = this.lifeMatrix(true);
 		
 		let colors = {
 			aliveColor: "#90c482",
@@ -106,8 +125,8 @@ class Game extends React.Component{
 
 	render(){
 		return (
-			<div id="game">
-				<div id="board">
+			<div id="gol-game">
+				<div id="gol-board">
 					<Board 
 						colors = {this.state.colors}
 						lifeMatrix = {this.state.lifeMatrix}
@@ -115,16 +134,68 @@ class Game extends React.Component{
 					/>
 				</div>
 
-				<div id="controls">
+				<div id="gol-controls">
 					<Controls
 						running = {this.state.running}
 						onRunningPauseClick = {()=>this.handleRunningPauseClick()}
 						onClearClick = {()=>this.handleClearClick()}
+						onSpeedChange = {(event)=>this.handleSpeedChange(event)}
 					/>
 				</div>
 			</div>
 		);
 			
+	}
+
+	increment_decrement_Neighbors(y, x, matrix, incrementOrDecrement){
+		var count = 0;
+		for ( let i = y-1; i <= y+1; i ++){
+			for ( let j = x-1; j <= x+1; j ++){
+				if ( !((i == y) && (j == x)) ){
+					if ( (j >= 0) && (j < matrix[y].length) ){
+						if ( (i >= 0) && (i < matrix.length) ){
+							var incOrDec = incrementOrDecrement? 1 : -1;
+							matrix[i][j].neighborCount += incOrDec;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	lifeMatrix(random){
+		var lifeMatrix = [];
+		for (var i = 0; i < this.height; i++ ){
+			var row = []
+			for ( var j = 0; j < this.width; j++ ){
+				let alive = false;
+				if(random) alive = Math.random() > 0.7;
+				let neighborCount = 0;
+				row.push({
+					alive: alive,
+					neighborCount: neighborCount
+				});
+			}
+			lifeMatrix.push(row);
+		}
+		for (var i = 0; i < this.height; i++ ){
+			for ( var j = 0; j < this.width; j++ ){
+				lifeMatrix[i][j].neighborCount = this.countNeighbors(i, j, lifeMatrix);
+			}
+		}
+		return lifeMatrix;
+	}
+
+	delay = 1000;
+	percentDelay = 0.5;
+	handleSpeedChange(event){
+		let percentDelay = event.target.value * 0.01;
+		console.log(`Speed changed to ${percentDelay*100}%`);
+		this.percentDelay = percentDelay;
+		if (this.state.running){
+			clearInterval(this.interval);
+			this.run();
+		}
 	}
 
 	countNeighbors(y, x, current){
@@ -134,7 +205,7 @@ class Game extends React.Component{
 				if ( !((i == y) && (j == x)) ){
 					if ( (j >= 0) && (j < current[y].length) ){
 						if ( (i >= 0) && (i < current.length) ){
-							count += current[i][j];
+							count += current[i][j].alive;
 						}
 					}
 				}
@@ -143,104 +214,114 @@ class Game extends React.Component{
 		return count;
 	}
 
+	deepCopyLifeMatrix(original){
+		let copy = new Array(original.length);
+
+		for (var i = 0; i < original.length; i ++){
+			let nextRow = new Array(original[0].length);
+			copy[i] = nextRow;
+			for (var j = 0; j < original[0].length; j ++){
+				let cell = original[i][j];
+				copy[i][j] = {alive:cell.alive, neighborCount:cell.neighborCount}
+			}
+		}
+		return copy;
+	}
+
 	nextMatrix(){
 		console.log("nextMatrix");
 		let current = this.state.lifeMatrix;
-		let next = [];
+		const t0 = performance.now();
+		let next = this.deepCopyLifeMatrix(current);
+		const t1 = performance.now();
+		console.log(`Copy Matrix: ${t1-t0}`);
 		
+		const t00 = performance.now();
 		for (let i = 0; i < current.length; i ++){
-			let nextRow = []
 			for (let j = 0; j < current[i].length; j ++){
-				let n_count = this.countNeighbors(i, j, current);
-				let alive = current[i][j]
-				
-				if ( alive ){
-					if ( n_count == 2 || n_count == 3 ) nextRow.push(true);
-					else nextRow.push(false);
+				let cell = current[i][j];
+				let nc = cell.neighborCount;
+
+				/*
+				// Cell is dead and stays dead (No Change)
+				if(!cell.alive && (nc != 3) ) {
+					continue;
 				}
-				else if ( !alive ) {
-					if (n_count == 3 ) nextRow.push(true);
-					else nextRow.push(false);
+				
+				// Cell is alive and stays alive (No Change)
+				if ( cell.alive && [2, 3].includes(nc) ){
+					continue;
+				}
+				*/
+				
+				// Cell is alive and becomes dead (Change)
+				if ( cell.alive && ![2, 3].includes(nc) ){
+					next[i][j].alive = false;
+					this.increment_decrement_Neighbors(i, j, next, false);
+					continue;
+				}
+
+				// Cell is dead and becomes alive (Change)
+				if ( !cell.alive && (nc == 3) ) {
+					next[i][j].alive = true;
+					this.increment_decrement_Neighbors(i, j, next, true);
+					continue;
 				}
 			}
-			next.push(nextRow)
 		}
+		const t01 = performance.now();
+		console.log(`New State: ${t01-t00}`);
 		
 		return next;
 	}
 
+
 	interval = null;
 
 	run(){
-		//console.log("running");
-		this.setState({ 
-			lifeMatrix: this.nextMatrix()
-		}, ()=>{
-			if (this.state.running){
-				setTimeout(()=>{this.run()}, 10);
-			}
-		});
-	}
-
-	run_test(){
 		this.setState({running: true});
 
 		this.interval = setInterval(() => {
+			const t = performance.now();
 			this.setState({
 				lifeMatrix: this.nextMatrix()
-			})
-		}, 100);
-
-		console.log("run");
-		//return interval;
+			});
+			const t1 = performance.now();
+			console.log(`Render State: ${t1-t}`);
+		}, this.delay * this.percentDelay);
+		console.log("run clicked");
 	}
+
 	pause(){
 		clearInterval(this.interval);
 		this.setState({running:false});
-		console.log("paused");
+		console.log("paused clicked");
 	}
 
 	handleRunningPauseClick(){
-		/*
-		//let running = this.state.running;
-		//if (running) { this.pause() }
-		//else { this.run_test() }
-		
-		let running = this.state.running;
-		this.setState({ running: !running }, ()=>{
-			if ( this.state.running ){
-				//this.run();
-				this.interval = this.run_test();
-			} else {
-				this.pause();
-			}
-		});
-		*/
+		console.log("Run/Pause clicked")
 		let running = this.state.running;
 		if (running){
 			this.pause();
 		}else{
-			this.run_test();
+			this.run();
 		}
-		
 	}
 
 	handleClearClick(){
-		console.log("clear");
+		console.log("clear clicked");
 		this.pause();
-		let blank = Array(this.state.lifeMatrix.length).fill(null).map(x => Array(this.state.lifeMatrix[0].length).fill(false));
-		this.setState({lifeMatrix: blank});
+		//let blank = Array(this.state.lifeMatrix.length).fill(null).map(x => Array(this.state.lifeMatrix[0].length).fill({alive:false, neighborCount:0}));
+		this.setState({lifeMatrix: this.lifeMatrix()});
 	}
 
 	handleSquareClick(x, y){
 		let lifeMatrix = this.state.lifeMatrix;
-		//console.log(this.countNeighbors(y, x, lifeMatrix))
-		lifeMatrix[y][x] = !lifeMatrix[y][x];
-		this.setState({
-			lifeMatrix: lifeMatrix,
-		}, ()=>{
-			console.log(this.countNeighbors(y, x, this.state.lifeMatrix));
-		});
+		let neighbors = lifeMatrix[y][x].neighborCount;
+		console.log(`Square(${x},${y}) clicked, ${neighbors} neighbors`)
+		lifeMatrix[y][x].alive = !lifeMatrix[y][x].alive;
+		this.increment_decrement_Neighbors(y, x, lifeMatrix, lifeMatrix[y][x].alive);
+		this.setState({lifeMatrix: lifeMatrix});
 	}
 }
 
